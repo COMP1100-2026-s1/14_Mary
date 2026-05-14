111d176540973b6e22a792ef6d5f4235718d6dd4
ernestliang <jitweiliang@gmail.com>
2026-05-15 00:10:28 +1000

    Progress on Degree Pages

--- ITERATION_3/csse3210-prototype/src/pages/degrees/DegreeTemplate.jsx
+++ ITERATION_3/csse3210-prototype/src/pages/degrees/DegreeTemplate.jsx
@@ -1,17 +1,53 @@
 
 import React, { useState, useEffect } from 'react';
-import { useParams } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
 
+function message() {
+    alert("Hi there!")
+}
 
 function DegreeTemplate() {
 
-    const { id } = useParams();
+    const location = useLocation();
+    const { degreeMajorData, degreeName } = location.state || {};
 
     return (
-        <>
-            <h1>Degree Template Here</h1>
-            <h2>Selected Degree ID: {id}</h2>
-        </>
+        <div className="container-fluid">
+            <br/>
+            {/* Title */}
+            <div className="container">
+                <h1 className="text-center">{degreeName} Study Plan</h1>
+                <h4>Major: {degreeMajorData.majorName}</h4>
+            </div>
+
+            {/* Course List */}
+            <div className="container">
+                Recommended Courses:
+                {Object.entries(degreeMajorData.recommendedCourses).map(([key, value]) => (
+                        <div className="row">
+                            <div className="col">
+                                {key}: 
+                            </div>
+                            {Array.from({ length: 4 }).map((_, index) => (
+                                <div className="col" key={index}>
+                                    {value[index] || "-"}
+                                </div>
+                            ))}
+                            {/* {value.map((course, index) => (
+                                <div className="col">
+                                    {course}
+                                </div>
+                            ))} */}
+                        </div>
+                    ))}
+            </div>
+
+            {/* Semester Table */}
+            <div className="container">
+                
+            </div>
+
+        </div>
     )
 }
 
